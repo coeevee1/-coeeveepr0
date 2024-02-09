bc0cadf1704b26b670e0fc580235196db54e5005
coeevee1 <120228155+coeevee1@users.noreply.github.com>
2024-02-09 08:09:25 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+x = float(input())
+if (x == int):
+    x = int(input())
+print("Type Yes or No for dual cicle please use capitals")
+Awnser = input()
+Estop = 2
+
+import turtle
+
+t = turtle.Turtle()
+
+for c in ['red', 'green', 'yellow', 'blue']:
+    for (i) in range(Estop):
+        t.speed(100)
+        t.circle(x, x)
+        t.left(x)
+        t.forward(x)
+        t.penup
+        t.circle(x, x)
+        t.left(x)
+        t.pendown
+        t.right(x)
+        t.forward(x)
+        t.forward(x)
+        t.right(x)
+        t.circle(x, x)
+        t.right(x)
+        t.forward(x)
+        t.left(x)
+        t.circle(x, x)
+        t.forward(x)
+        t.circle(x, x)
+        t.left(x)
+        t.forward(x)
+        if Awnser == ("Yes"):
+            t.speed(100)
+            t.right(x)
+            t.forward(x)
+            t.forward(x)
+            t.right(x)
+            t.circle(x, x)
+            t.right(x)
+            t.forward(x)
+            t.left(x)
+            t.circle(x, x)
+            t.forward(x)
+            t.circle(x, x)
+            t.left(x)
+            t.forward(x)
+            t.right(x)
+            t.forward(x)
+            t.forward(x)
+            t.right(x)
+            t.circle(x, x)
+            t.right(x)
+            t.forward(x)
+            t.left(x)
+            t.circle(x, x)
+            t.forward(x)
+            Estop = Estop - 1
+            if Estop == 1:
+                print("would you like to continue if so enter how many times")
+                Estop = int(input())
+                if (Estop == float):
+                    Estop = float(input())
